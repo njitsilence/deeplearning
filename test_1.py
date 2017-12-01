8cb3912c50119f4fe9194b77c835e4480d73fcc9
John <249446643@qq.com>
2017-12-01 15:37:37 +0800

    Initial commit

---
+++ test_1.py
@@ -0,0 +1,7 @@
+from wq_util import *
+
+
+x = np.array([
+    [9, 2, 5, 0, 0],
+    [7, 5, 0, 0 ,0]])
+print("softmax(x) = " + str(softmax(x)))
---
+++ wq_util.py
@@ -0,0 +1,102 @@
+import math
+import numpy as np
+
+# basic_sigmoid - only support the real number
+def basic_sigmoid(x):
+    """
+    Compute sigmoid of x.
+
+    Arguments:
+    x -- A scalar
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1 / (1 + math.exp(-x))
+    return s
+
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1 / (1 + np.exp(-x))
+    return s
+
+
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+    s = sigmoid(x)
+    ds = s * (1 - s)
+    return ds
+
+
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+
+    v = image.reshape((image.shape[0] * image.shape[1] * image.shape[2], 1))
+    return v
+
+
+def normalizeRows(x):
+    """
+    Implement a function that normalizes each row of the matrix x (to have unit length).
+
+    Argument:
+    x -- A numpy matrix of shape (n, m)
+
+    Returns:
+    x -- The normalized (by row) numpy matrix. You are allowed to modify x.
+    """
+
+    ### START CODE HERE ### (≈ 2 lines of code)
+    # Compute x_norm as the norm 2 of x. Use np.linalg.norm(..., ord = 2, axis = ..., keepdims = True)
+    x_norm = np.linalg.norm(x, axis=1, keepdims=True)
+
+    x = x / x_norm
+    return x
+
+
+def softmax(x):
+    """
+    Calculates the softmax for each row of the input x.
+    Your code should work for a row vector and also for matrices of shape (n, m).
+    Argument:
+    x -- A numpy matrix of shape (n,m)
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (n,m)
+    """
+
+    # Apply exp() element-wise to x. Use np.exp(...).
+    x_exp = np.exp(x)
+
+    # Create a vector x_sum that sums each row of x_exp. Use np.sum(..., axis = 1, keepdims = True).
+    x_sum = np.sum(x_exp, axis=1, keepdims=True)
+
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    s = x_exp / x_sum
+
+    ### END CODE HERE ###
+
+    return s
